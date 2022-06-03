43534641549b80ec813cde3f2871d88158b1b8e4
omkar-umbc <90357608+omkar-umbc@users.noreply.github.com>
2022-06-03 01:32:07 -0400

    Update bat_omkar.sh

--- bat_omkar.sh
+++ bat_omkar.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=mrc
+#SBATCH --job-name=mrc3
 #SBATCH --mail-user=omkark1@umbc.edu
 #SBATCH --mem=80G
 #SBATCH --gres=gpu:8
 #SBATCH --time=3-12:00:00
 #SBATCH --constraint=rtx_2080
-#SBATCH --error=mrc2.err
-#SBATCH --output=mrc2.out
+#SBATCH --error=mrc3.err
+#SBATCH --output=mrc3.out
 
 source activate mrcnn
 python /nfs/rs/oates/users/omkark1/USound/Source/USound/samples/shapes/usoundmrcnn.py
