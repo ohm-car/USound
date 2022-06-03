5beb2c77e4ef6650e47c95d671a2ec138c08723c
omkar-umbc <90357608+omkar-umbc@users.noreply.github.com>
2022-06-03 04:45:19 -0400

    Update bat_omkar.sh

--- bat_omkar.sh
+++ bat_omkar.sh
@@ -2,10 +2,10 @@
 
 #SBATCH --job-name=mrc5
 #SBATCH --mail-user=omkark1@umbc.edu
-#SBATCH --mem=96G
-#SBATCH --gres=gpu:4
+#SBATCH --mem=48G
+#SBATCH --gres=gpu:1
 #SBATCH --time=3-12:00:00
-#SBATCH --constraint=rtx_6000
+#SBATCH --constraint=rtx_8000
 #SBATCH --error=mrc5.err
 #SBATCH --output=mrc5.out
 
