3086ac4ce05b89cdca5db9db825eded0fb713319
omkar-umbc <90357608+omkar-umbc@users.noreply.github.com>
2022-06-06 18:12:22 -0400

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2342,10 +2342,10 @@ class MaskRCNN(object):
 
         # Callbacks
         callbacks = [
-            keras.callbacks.TensorBoard(log_dir=self.log_dir,
-                                        histogram_freq=0, write_graph=True, write_images=False),
+#             keras.callbacks.TensorBoard(log_dir=self.log_dir,
+#                                         histogram_freq=0, write_graph=True, write_images=False),
             keras.callbacks.ModelCheckpoint(self.checkpoint_path,
-                                            verbose=0, save_weights_only=True),
+                                            verbose=0, save_weights_only=True, save_freq = 2000),
         ]
 
         # Add custom callbacks to the list
