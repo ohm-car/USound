73946137421e273e78d96ae922efb103f41fb291
omkar-umbc <90357608+omkar-umbc@users.noreply.github.com>
2022-06-14 12:15:20 -0400

    Update checkpoint frequency

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2345,7 +2345,7 @@ class MaskRCNN(object):
 #             keras.callbacks.TensorBoard(log_dir=self.log_dir,
 #                                         histogram_freq=0, write_graph=True, write_images=False),
             keras.callbacks.ModelCheckpoint(self.checkpoint_path,
-                                            verbose=0, save_weights_only=True, save_freq = 2000),
+                                            verbose=0, save_weights_only=True, save_freq = 5000),
         ]
 
         # Add custom callbacks to the list
