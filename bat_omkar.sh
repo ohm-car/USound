f414602400f9f7005c98ceb1f2c633db6346792f
omkar-umbc <90357608+omkar-umbc@users.noreply.github.com>
2022-06-26 23:34:15 -0400

    Update bat_omkar.sh

--- bat_omkar.sh
+++ bat_omkar.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=48G
 #SBATCH --gres=gpu:1
 #SBATCH --constraint=rtx_8000
-#SBATCH --time=7-00:00:00
+#SBATCH --time=8-00:00:00
 #SBATCH --error=mrcrn152b.err
 #SBATCH --output=mrcrn152b.out
 
