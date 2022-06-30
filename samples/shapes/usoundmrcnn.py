8c40c809961024492dbe9f4e9934bb27220f5b2b
omkar-umbc <90357608+omkar-umbc@users.noreply.github.com>
2022-06-29 22:49:41 -0400

    Update usoundmrcnn.py

--- samples/shapes/usoundmrcnn.py
+++ samples/shapes/usoundmrcnn.py
@@ -587,7 +587,7 @@ elif init_with == "last":
 # # train by name pattern.
 model.train(dataset_train, dataset_val, 
             learning_rate=config.LEARNING_RATE / 8,
-            epochs=100, 
+            epochs=2, 
             layers="all")
 
 
